e57eb7f408b40d59b0a6a7d9f4ca7996e1ba4f54
Juan Arroyave <arroyavejuan2111@gmail.com>
2024-09-13 21:29:53 -0500

    Prueba ok, el algoritmo muestra el valor de las comisiones segun ventas, y el sueldo final.

--- Algoritmo1_Ventas/Program.cs
+++ Algoritmo1_Ventas/Program.cs
@@ -22,17 +22,15 @@ public class Program
         Console.Write("Ingrese el valor de su tercer venta: ");
         decimal venta3 = Convert.ToDecimal(Console.ReadLine());
 
-        decimal comision1 = venta1 * (10 / 100);
-        decimal comision2 = venta2 * (10 / 100);
-        decimal comision3 = venta3 * (10 / 100);
+        decimal comision1 = (venta1 * 10 / 100);
+        decimal comision2 = (venta2 * 10 / 100);
+        decimal comision3 = (venta3 * 10 / 100);
         comisionTotal = comision1 + comision2 + comision3;
 
         sueldoTotal = sueldoBasico + comisionTotal;
 
-        Console.WriteLine($"El valor de las comisiones recibidaS por la venta 1 son de :{comision1:C0}, por la venta 2 son de :{comision2:C0}, y por la venta 3 son de :{comision3:C0}");
+        Console.WriteLine($"El valor de las comisiones recibidas por la venta 1 son de :{comision1:C0}, por la venta 2 son de :{comision2:C0}, y por la venta 3 son de :{comision3:C0}");
         Console.WriteLine($"Su sueldo total es de : {sueldoTotal:C0}");
-
-
 
     }
 }
